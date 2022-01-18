eb8be819b42c8ddae322a2165c97df17a9c2d211
DracoMithril <dracomithril@gmail.com>
2022-01-18 01:34:47 +0100

    fix: import

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+import styles from './home.module.scss';
 
 const Home: NextPage = () => {
   return (
